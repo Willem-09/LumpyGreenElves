10ad58d5eb58f687a2e7b7cc0ebaa4177e60ca0e
Willem Pretorius <127272864+Willem-09@users.noreply.github.com>
2023-03-22 02:37:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+Name = "Willem"
+Number = 24
+print(Number-4)
+Pizza_shape = "Vegetable"
+print(Pizza_shape)
+#Variable1
+Vegetable = "POTATO"
+print(Vegetable)
+Vegetable = "broccoli"
+#Variable2
+print(Vegetable)
